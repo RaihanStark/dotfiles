0482814c09942edc43b94f956c44f6378fa05812
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2024-09-05 16:49:00 +0700

    Update keymaps.lua

--- .config/lvim/lua/user/keymaps.lua
+++ .config/lvim/lua/user/keymaps.lua
@@ -1,6 +1,12 @@
 -- Keymaps for Harpoon
-lvim.keys.normal_mode['hl'] = '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>'
-lvim.keys.normal_mode['hx'] = ':lua require("harpoon.mark").add_file()<CR>'
+lvim.builtin.which_key.mappings['h'] = {
+  name = '+Harpoon',
+  l = { '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', 'Harpoon List' },
+  x = { '<cmd>lua require("harpoon.mark").add_file()<CR>', 'Mark File' },
+  n = { '<cmd>lua require("harpoon.ui").nav_next()<CR>', 'Next File' },
+  p = { '<cmd>lua require("harpoon.ui").nav_prev()<CR>', 'Previous File' },
+}
+
 lvim.keys.normal_mode['<S-Up>'] = ':lua require("harpoon.ui").nav_next()<CR>'
 lvim.keys.normal_mode['<S-Down>'] = ':lua require("harpoon.ui").nav_prev()<CR>'
 
@@ -9,12 +15,28 @@ lvim.keys.normal_mode['<leader>ss'] = { ':lua require("persistence").load({ last
 
 -- Keymaps for lspsaga
 lvim.lsp.buffer_mappings.normal_mode['<leader>la'] = { ':Lspsaga code_action<CR>', { desc = 'Code action' } }
-
--- TODO: Add p group so the which key is named "Peek"
-lvim.keys.normal_mode['<leader>lpd'] = { ':Lspsaga peek_definition<CR>', { desc = 'Peek definition' } }
-lvim.keys.normal_mode['<leader>lpD'] = { ':Lspsaga peek_type_definition<CR>', { desc = 'Peek type definition' } }
-lvim.keys.normal_mode['<leader>lpf'] = { ':Lspsaga finder<CR>', { desc = 'LSP finder' } }
-
 lvim.lsp.buffer_mappings.normal_mode['K'] = { ':Lspsaga hover_doc<CR>', { desc = 'Hover documentation' } }
 lvim.lsp.buffer_mappings.normal_mode['gd'] = { ':Lspsaga goto_definition<CR>', { desc = 'Goto definition' } }
 lvim.lsp.buffer_mappings.normal_mode['<leader>lr'] = { ':Lspsaga rename<CR>', { desc = 'Rename' } }
+
+-- Keymaps for find files and recent files
+-- Todo: add F group so the which key is named "File"
+lvim.builtin.which_key.mappings['f'] = {
+  name = "+File",
+  f = { ':Telescope find_files<CR>', "Find files" },
+  r = { ':Telescope oldfiles<CR>', "Recent files" },
+}
+
+-- Peek keymaps
+lvim.builtin.which_key.mappings['lp'] = {
+  name = '+Peek',
+  d = { ':Lspsaga peek_definition<CR>', 'Peek definition' },
+  D = { ':Lspsaga peek_type_definition<CR>', 'Peek type definition' },
+  f = { ':Lspsaga finder<CR>', 'LSP finder' },
+}
+
+lvim.keys.normal_mode['<leader>sT'] = { ':TodoTelescope<CR>', { desc = "Todo" } }
+
+-- Keymaps for scrolling and center cursor
+lvim.keys.normal_mode['<M-d>'] = "<C-b>zz" -- Scroll up
+lvim.keys.normal_mode['<M-b>'] = "<C-d>zz" -- Scroll down
